a4daf38f53f1514257019a2497612f0d4437a67b
Allison Turner <hermione2259@gmail.com>
2016-01-21 22:58:55 -0500

    Basic stuff

--- 2016-Robot/src/org/usfirst/frc/team88/robot/subsystems/Intake.java
+++ 2016-Robot/src/org/usfirst/frc/team88/robot/subsystems/Intake.java
@@ -1,14 +1,33 @@
 package org.usfirst.frc.team88.robot.subsystems;
 
+import org.usfirst.frc.team88.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class Intake extends Subsystem {
-    
+    private final CANTalon outerRollerTalon, innerRollerTalon;
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
+    public Intake(){
+    	outerRollerTalon = new CANTalon(RobotMap.frontRollerMotorController);
+    	innerRollerTalon = new CANTalon(RobotMap.innerRollerMotorController);
+    }
+    
+    public void RollOuterRoller(double direction){
+    	
+    }
+    
+    public void RollInnerRoller(double direction){
+    	
+    }
+    
+    public void RollBothRollers(double direction){
+    	
+    }
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
